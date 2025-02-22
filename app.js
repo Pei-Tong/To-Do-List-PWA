68b7bf3e59b8814decf56daf7c76309adf650f6b
Pei-Tong <153776311+Pei-Tong@users.noreply.github.com>
2025-02-21 23:52:08 -0500

    firebaseconfig & service work modify

--- app.js
+++ app.js
@@ -42,3 +42,20 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getAnalytics(app);
+
+
+const sw = new URL('/To-Do-List-PWA/service-worker.js', import.meta.url);
+
+if ('serviceWorker' in navigator) {
+  const s = navigator.serviceWorker;
+
+  s.register(sw.href, {
+    scope: '/To-Do-List-PWA/',
+  })
+    .then(() => 
+      console.log('Service Worker Registered for scope:', sw.href, 'with', import.meta.url)
+    )
+    .catch((err) => 
+      console.error('Service Worker Error:', err)
+    );
+}
