531ff2b67bfda512f86e42551915ae0e5902cf38
Pei-Tong <153776311+Pei-Tong@users.noreply.github.com>
2025-02-22 00:27:48 -0500

    modif firebase config

--- app.js
+++ app.js
@@ -23,12 +23,8 @@ taskList.addEventListener("click", (e) => {
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { doc, getDocs, addDoc, updateDoc, getFirestore, collection } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAIG7xk6369LrCe0OIiDoPZHZuMcUikuc4",
   authDomain: "todo-list-a5d26.firebaseapp.com",
