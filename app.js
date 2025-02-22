3b2e9f690d5e4c998c5d005728b22b16b5c27989
Pei-Tong <153776311+Pei-Tong@users.noreply.github.com>
2025-02-21 23:28:24 -0500

    firebaseconfig & service work modify

--- app.js
+++ app.js
@@ -19,3 +19,26 @@ taskList.addEventListener("click", (e) => {
     e.target.remove();
   }
 });
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAIG7xk6369LrCe0OIiDoPZHZuMcUikuc4",
+  authDomain: "todo-list-a5d26.firebaseapp.com",
+  projectId: "todo-list-a5d26",
+  storageBucket: "todo-list-a5d26.firebasestorage.app",
+  messagingSenderId: "668975418178",
+  appId: "1:668975418178:web:34c993da77dd83c116d6db",
+  measurementId: "G-EM5RQTDPS8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getAnalytics(app);
